734ac3d2fa3f0014fd2b43dadd40ec932c4c0f96
bbtechpro <billy.barber@gmail.com>
2026-05-02 22:13:09 -0400

    import { BrowserRouter, Routes, Route } from 'react-router-dom'; import { AuthProvider } from './components/AuthContext'; import { ProtectedRoute } from './components/ProtectedRoute' import { Navbar } from './components/ConditionalUi';

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext';
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { Navbar } from './components/ConditionalUi';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
