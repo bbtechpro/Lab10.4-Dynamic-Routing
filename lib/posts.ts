22debd57ab5d3d78b7bdebeb933455654ec286dc
bbtechpro <billy.barber@gmail.com>
2026-05-02 21:05:40 -0400

    Import Post from types and declare Posts array.

---
+++ lib/posts.ts
@@ -0,0 +1,14 @@
+import { Post } from "./types";
+
+export const posts: Post[] = [
+    {
+        id: 1,
+        title: "First Post",
+        content: "This is the content of the first post."
+    },
+    {
+        id: 2,
+        title: "Second Post",
+        content: "This is the content of the second post."
+    }
+];
