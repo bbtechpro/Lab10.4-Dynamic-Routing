cb7d6648644623531663a15fbd9c1d600cbf0065
bbtechpro <billy.barber@gmail.com>
2026-05-02 22:36:24 -0400

    Moved functionality to Blogpost.ts

---
+++ src/lib/posts.ts
@@ -0,0 +1,16 @@
+import type { Post } from '../components/types';
+
+export const posts: Post[] = [
+  {
+    id: 1,
+    slug: 'my-first-post',
+    title: 'My First Post',
+    content: 'This is the content of the first post.',
+  },
+  {
+    id: 2,
+    slug: 'another-update',
+    title: 'Another Update',
+    content: 'This is the content of the second post.',
+  },
+];
