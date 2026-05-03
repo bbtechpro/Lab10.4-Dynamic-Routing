86132550b2c0f3eb76af2db3664dcd968de66693
bbtechpro <billy.barber@gmail.com>
2026-05-02 22:59:33 -0400

    Establiosh react routes to elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
-import { ProtectedRoute } from './components/ProtectedRoute'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AdminDashboard } from './components/AdminDashboard';
+import { AuthProvider } from './components/AuthProvider';
+import { BlogIndex } from './components/BlogIndex';
+import { BlogPost } from './components/BlogPost';
 import { Navbar } from './components/ConditionalUi';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -10,14 +15,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogIndex />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/login" element={<Login />} />
-          <Route 
-            path="/admin" 
+          <Route
+            path="/admin"
             element={
               <ProtectedRoute>
                 <AdminDashboard />
               </ProtectedRoute>
-            } 
+            }
           />
         </Routes>
       </BrowserRouter>
